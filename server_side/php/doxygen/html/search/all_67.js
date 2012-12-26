a7ccd917f5a552bed5c7255869587d5b60538b80
Christos Pontikis <pontikis@gmail.com>
2012-12-26 17:32:29 +0200

    Update docs

--- server_side/php/doxygen/html/search/all_67.js
+++ server_side/php/doxygen/html/search/all_67.js
@@ -1,6 +1,4 @@
 var searchData=
 [
-  ['get_5fconn',['get_conn',['../dc/d37/classjui__filter__rules.html#a44068bd49063ef1bc0ad423a944657b2',1,'jui_filter_rules']]],
-  ['get_5frdbms',['get_rdbms',['../dc/d37/classjui__filter__rules.html#aacf38a5d68b5daf336a87aaf5651e544',1,'jui_filter_rules']]],
-  ['get_5fusepreparedstatements',['get_usePreparedStatements',['../dc/d37/classjui__filter__rules.html#a6dc1f1e954ee2af5e37e45eb420aea3f',1,'jui_filter_rules']]]
+  ['get_5flast_5ferror',['get_last_error',['../dc/d37/classjui__filter__rules.html#abfc037d7134d1a1f9c1fd5b4970633a1',1,'jui_filter_rules']]]
 ];
