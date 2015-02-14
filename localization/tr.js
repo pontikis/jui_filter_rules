9948115b2f829caa6b0401b540dddc0b5c7ed3c3
Christos Pontikis <pontikis@gmail.com>
2015-02-14 22:58:44 +0200

    turkish translation of jui_filter_rules - close #17

---
+++ localization/tr.js
@@ -0,0 +1,57 @@
+/**
+ * jui_filter_rules v1.0.6 simple localization - TURKISH
+ *
+ * by Şimşek Mert www.simsekmert.com
+ *
+ * DO NOT CHANGE this file, as it will be overwritten in next update.
+ * To use different values, write and use a similar structure js file.
+ *
+ */
+var rsc_jui_fr = {
+
+    no_filters_found: "Herhangi bir kural tanımlanmadı.",
+
+    rules_group_AND: "Tüm kuralları uygula (AND)",
+    rules_group_OR: "Herhangi bir kuralı uygula (OR)",
+
+    rule: "kural",
+    group: "grup",
+    tools_please_select: "&raquo;",
+    rule_insert_before: "Önüne Ekle",
+    rule_insert_after: "Arkasına Ekle",
+    rule_insert_inside: "İçerisine Ekle",
+    rule_clear: "Temizle",
+    rule_delete: "Sil",
+    group_insert_before: "Öncesine Ekle",
+    group_insert_after: "Arkasına Ekle",
+    group_insert_inside: "İçerisine Ekle",
+    group_delete: "Sil",
+
+    filter_please_select: '&raquo; Seç',
+
+    operator_equal: 'eşittir',
+    operator_not_equal: 'eşit değildir',
+    operator_in: 'içinde',
+    operator_not_in: 'içinde değil',
+    operator_less: 'küçüktür',
+    operator_less_or_equal: 'küçük veya eşittir',
+    operator_greater: 'büyüktür',
+    operator_greater_or_equal: 'büyük veya eşittir',
+    operator_between: 'arasında',
+    operator_not_between: 'arasında değil',
+    operator_begins_with: 'ile başlayan',
+    operator_not_begins_with: 'ile başlamayan',
+    operator_contains: 'içerir',
+    operator_not_contains: 'içermez',
+    operator_ends_with: 'ile biten',
+    operator_not_ends_with: 'ile bitmeyen',
+    operator_is_empty: 'boş',
+    operator_is_not_empty: 'boş değil',
+    operator_is_null: 'tanımsız',
+    operator_is_not_null: 'tanımsız değil',
+
+    error_no_value_given: "Değer verilmemiş",
+    error_invalid_number: "Rakam değeri olmayan",
+    error_invalid_datetime: "Geçersiz tarih biçimi",
+    error_converting_value: "Hata döndüren değer"
+};
